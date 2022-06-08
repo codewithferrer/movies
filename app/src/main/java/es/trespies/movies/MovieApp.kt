9bb2e1c54bb1cc46e59cb2153f53249cf479d5c0
danielferrer <danybarco@gmail.com>
2022-06-08 07:03:21 +0200

    Prepare for Hilt DI

---
+++ app/src/main/java/es/trespies/movies/MovieApp.kt
@@ -0,0 +1,16 @@
+package es.trespies.movies
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class MovieApp: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        Timber.plant(Timber.DebugTree())
+    }
+
+}
